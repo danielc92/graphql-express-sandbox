7d5b92d68af16ee5252b129701c3a46cd224fa8e
Daniel Corcoran <danielc.92@outlook.com>
2020-03-08 18:13:22 +1100

    Added booking queries

--- schema/index.js
+++ schema/index.js
@@ -1,54 +1,19 @@
-const userFields = require("./user")
-const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLID,
-  GraphQLSchema,
-  GraphQLList,
-  GraphQLNonNull,
-  GraphQLError
-} = require("graphql")
+const { GraphQLObjectType, GraphQLSchema } = require("graphql")
+const { userQueries, userMutations } = require("./user")
+const { bookingQueries, bookingMutations } = require("./bookings")
 
-const { UserType } = require("../types/index")
-const {userQueries, userMutations} = require('./user')
 const RootQuery = new GraphQLObjectType({
   name: "Query",
-  fields: userQueries
+  fields: {
+    ...userQueries,
+    ...bookingQueries
   }
 })
 
 const RootMutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
-    insertUser: {
-      type: UserType,
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLID) },
-        first_name: { type: new GraphQLNonNull(GraphQLString) },
-        last_name: { type: new GraphQLNonNull(GraphQLString) }
-      },
-      resolve(parent, args) {
-        const { id, first_name, last_name } = args
-
-        // Check if user exists first
-        const exists = Users.some(u => u.id === id)
-        if (exists) return new GraphQLError("This user id already exists")
-
-        let user = { id, first_name, last_name }
-        Users.push(user)
-        return user
-      }
-    },
-    findUser: {
-      type: UserType,
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLID) }
-      },
-      resolve(parent, args) {
-        const result = Users.find(u => String(u.id) === args.id)
-        return result
-      }
-    }
+    ...userMutations
   }
 })
 
