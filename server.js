b9affa58e2d434e7373218f4aef8ececb7d1d1eb
Daniel Corcoran <danielc.92@outlook.com>
2020-03-08 14:13:23 +1100

    Added Array return

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const {buildSchema} = require('graphql')
+const express = require("express")
+const graphqlHTTP = require("express-graphql")
+const { buildSchema } = require("graphql")
 
 const app = express()
 
-
-
 const schema = buildSchema(`
 type Query {
-    hello: String
+    hello: [String]
 }`)
 
-app.use('/graphql', graphqlHTTP({
+const root = {
+  hello: () => ["helo", "world"]
+}
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true
-}))
+  })
+)
 
-app.listen(4000, ()=> console.log('Server now running, access on localhost:4000/graphql'))
+app.listen(4000, () =>
+  console.log("Server now running, access on localhost:4000/graphql")
+)
