4ac10968b53cd6cd6a27ac80548f6f8bf954d1b8
Daniel Corcoran <danielc.92@outlook.com>
2020-03-08 18:02:40 +1100

    Fix user file

---
+++ schema/user/index.js
@@ -0,0 +1,55 @@
+const {
+  GraphQLString,
+  GraphQLID,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLError
+} = require("graphql")
+const { UserType } = require("../../types")
+const Users = require("../../data/users.json")
+
+const userQueries = {
+  users: {
+    type: GraphQLList(UserType),
+    resolve(parent, args) {
+      return Users
+    }
+  }
+}
+
+const userMutations = {
+  insertUser: {
+    type: UserType,
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLID) },
+      first_name: { type: new GraphQLNonNull(GraphQLString) },
+      last_name: { type: new GraphQLNonNull(GraphQLString) }
+    },
+    resolve(parent, args) {
+      const { id, first_name, last_name } = args
+
+      // Check if user exists first
+      const exists = Users.some(u => u.id === id)
+      if (exists) return new GraphQLError("This user id already exists")
+
+      let user = { id, first_name, last_name }
+      Users.push(user)
+      return user
+    }
+  },
+  findUser: {
+    type: UserType,
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLID) }
+    },
+    resolve(parent, args) {
+      const result = Users.find(u => String(u.id) === args.id)
+      return result
+    }
+  }
+}
+
+module.exports = {
+  userMutations,
+  userQueries
+}
