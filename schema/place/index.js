7093c4470b50b0a1da40ebdf5a93295cf5c7125c
Daniel Corcoran <danielc.92@outlook.com>
2020-03-09 12:43:12 +1100

    Added placeMutation create place

--- schema/place/index.js
+++ schema/place/index.js
@@ -1,6 +1,6 @@
 const { PlaceListType } = require("../../types")
-const Places = require("../../data/places.json")
-const { GraphQLString } = require("graphql")
+let Places = require("../../data/places.json")
+const { GraphQLID, GraphQLString } = require("graphql")
 
 const placeQueries = {
   places: {
@@ -15,7 +15,28 @@ const placeQueries = {
   }
 }
 
-const placeMutations = {}
+const placeMutations = {
+  createPlace: {
+    type: PlaceListType,
+    args: {
+      id: { type: GraphQLID },
+      name: { type: GraphQLString },
+      state: { type: GraphQLString },
+      country: { type: GraphQLString }
+    },
+    resolve(parent, args) {
+      const place = {
+        id: Places.length + 1,
+        owner_id: args.owner_id,
+        name: args.name,
+        state: args.state,
+        country: args.country
+      }
+      Places = [...Places, place]
+      return Places
+    }
+  }
+}
 
 module.exports = {
   placeQueries,
